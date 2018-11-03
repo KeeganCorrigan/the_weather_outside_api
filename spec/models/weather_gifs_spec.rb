2b4050ec3f8c2b6130bf2731b6de8ae0f6f07fcb
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-11-03 11:14:18 -0600

    [#161697623]Add model test for WeatherGif

---
+++ spec/models/weather_gifs_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe WeatherGif, type: Model do
+  context "validations" do
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:type) }
+    it { should validate_presence_of(:image_url) }
+    it { should validate_presence_of(:temperature_range) }
+  end
+end
