d892df1075a2665757ec47a2c8cfa66e5ad01fe4
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-11-03 11:31:38 -0600

    [#161697627]Add sad path tests for bad weather request

--- spec/api/v1/weather/weather_get_spec.rb
+++ spec/api/v1/weather/weather_get_spec.rb
@@ -3,13 +3,26 @@ require "rails_helper"
 describe "weather API" do
   describe "GET /api/v1/weather" do
     it "returns limited current weather information and associated gif" do
-      get "/api/v1/weather", params: { latitude: "39.7392", longitude: "104.9903"}
+      get "/api/v1/weather", params: { latitude: 39.7392, longitude: 104.9903 }
 
       weather = JSON.parse(response.body, symbolize_names: true)
 
       expect(weather).to have_key(:image_url)
       expect(weather).to have_key(:summary)
       expect(weather).to have_key(:temperature)
+    end
+
+    it "returns 400 if latitude is not present" do
+      get "/api/v1/weather", params: { longitude: 104.9903 }
+
+      expect(response).to have_http_status(400)
+    end
+
+    it "returns 400 if longitude is not present" do
+      get "/api/v1/weather", params: { latitude: 39.7392 }
+
+      expect(response).to have_http_status(400)
+
     end
   end
 end
