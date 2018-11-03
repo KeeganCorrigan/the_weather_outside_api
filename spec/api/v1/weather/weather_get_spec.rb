c9a4e196cef00a43d51f45ae2c6020f4f8dded9e
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-11-03 11:31:38 -0600

    [#161697627]Add cassette for weather get spec]

--- spec/api/v1/weather/weather_get_spec.rb
+++ spec/api/v1/weather/weather_get_spec.rb
@@ -3,16 +3,16 @@ require "rails_helper"
 describe "weather API" do
   describe "GET /api/v1/weather" do
     it "returns limited current weather information and associated gif" do
-      create(:weather_gif, condition: "Overcast")
-      get "/api/v1/weather", params: { latitude: 39.7392, longitude: 104.9903 }
+      VCR.use_cassette("weather_feature_spec_current_weather_denver") do
+        create(:weather_gif, condition: "Overcast")
+        get "/api/v1/weather", params: { latitude: 39.7392, longitude: 104.9903 }
 
-      weather = JSON.parse(response.body, symbolize_names: true)
+        weather = JSON.parse(response.body, symbolize_names: true)
 
-      binding.pry
-
-      expect(weather).to have_key(:image_url)
-      expect(weather).to have_key(:summary)
-      expect(weather).to have_key(:apparent_temperature)
+        expect(weather).to have_key(:image_url)
+        expect(weather).to have_key(:summary)
+        expect(weather).to have_key(:apparent_temperature)
+      end
     end
 
     it "returns 400 if latitude is not present" do
