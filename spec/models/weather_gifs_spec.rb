6c10764e651d0afcac7b8fc60857428a7b6643c0
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-11-03 11:25:23 -0600

    [#161697623]Add weather gif model

--- spec/models/weather_gifs_spec.rb
+++ spec/models/weather_gifs_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe WeatherGif, type: Model do
+RSpec.describe WeatherGif, type: :model do
   context "validations" do
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:type) }
